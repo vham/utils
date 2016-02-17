ae43661f34dab554e94ebabb3e3b6a8ce9c24327
Victor Hugo <victorhugo.avila.cl@gmail.com>
2016-02-17 09:11:47 -0300

    pdf_label fixing bugs

--- label/pdf_label.php
+++ label/pdf_label.php
@@ -21,10 +21,7 @@ $result = curl_exec($curl);
 
 if (curl_getinfo($curl, CURLINFO_HTTP_CODE) == 200) {
     $file = fopen("labelx3.pdf", "w"); // change file name for PNG images
-    fwrite($file, $result);
-    fclose($file);
-    $file = fopen("labelx3.pdf", "a");
-    fwrite($file, $result);
+    fwrite($file, $result.$result);
     fclose($file);
 } else {
     print_r("Error: $result");
